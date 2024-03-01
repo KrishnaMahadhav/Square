79063f48dd80d8b44206ebbbe9bed60103858ea6
Krishnakumar Sankar <98452642+KrishnaMahadhav@users.noreply.github.com>
2024-03-01 13:53:16 +0530

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main 
+{
+  public static void main(String[] args) {
+    
+    Scanner objnum = new Scanner(System.in);
+    int Num = objnum.nextInt();
+    System.out.println(Num * Num);
+  }
+}
